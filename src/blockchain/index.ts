ae42069c8821610bc51483c88ed5d16ff28ca74a
sociogram-dev <dev@sociogram.org>
2025-09-05 12:52:08 +0300

    Added  Utils

--- src/blockchain/index.ts
+++ src/blockchain/index.ts
@@ -1,3 +1,21 @@
 export * from './shared'
 export * from './evm'
 export * from './solana'
+
+/**
+ * Supported wallet extensions that can be used for authentication,
+ * signing, or interacting with dApps.
+ *
+ * @enum {string}
+ *
+ * @property {string} Metamask      - MetaMask browser extension / mobile app.
+ * @property {string} WalletConnect - WalletConnect protocol for linking external wallets.
+ * @property {string} Phantom       - Phantom wallet (primarily for Solana).
+ * @property {string} Coinbase      - Coinbase Wallet extension / mobile app.
+ */
+export enum WalletExtension {
+  Metamask = 'metamask',
+  WalletConnect = 'walletconnect',
+  Phantom = 'phantom',
+  Coinbase = 'coinbase',
+}
